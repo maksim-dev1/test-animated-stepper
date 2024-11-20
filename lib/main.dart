85dccb2d9f841eab04c639c79b7b3d4e72d7e968
Maksim <progr9@pancir.it>
2024-11-20 12:07:17 +0500

    add CustomStepperV2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:custom_stepper/custom_stepper.dart';
+import 'package:custom_stepper/custom_stepper_v2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,6 +31,26 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int completedSteps = 1;
   int upcomingSteps = 4;
+
+  final List<bool> positions = List.generate(5, (index) => false);
+
+void toggleStep(bool value) {
+  setState(() {
+    for (int i = 1; i < positions.length - 1; i++) {
+      if (positions[i] == false && value == true) {
+        positions[i] = true;
+        break;
+      } else if (positions[i] == true && value == false) {
+        positions[i] = false;
+        break; 
+      }
+    }
+    print(positions);
+  });
+}
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,10 +61,22 @@ class _MyHomePageState extends State<MyHomePage> {
         padding: const EdgeInsets.all(20),
         child: Column(
           children: [
+            const Text('Stepper V1'),
+            const SizedBox(
+              height: 20,
+            ),
             CustomStepper(
               completedSteps: completedSteps,
               upcomingSteps: upcomingSteps,
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text('Stepper V2'),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomStepperV2(positions: positions),
             const Spacer(),
             Row(
               children: [
@@ -52,10 +85,13 @@ class _MyHomePageState extends State<MyHomePage> {
                     onPressed: () {
                       if (completedSteps > 1) {
                         setState(() {
+                          // 'Stepper V1'
                           completedSteps--;
                           upcomingSteps++;
+                          // 'Stepper V2'
                         });
                       }
+                      toggleStep(false);
                     },
                     child: const Text(
                       'Back',
@@ -71,10 +107,13 @@ class _MyHomePageState extends State<MyHomePage> {
                     onPressed: () {
                       if (upcomingSteps > 1) {
                         setState(() {
+                          // 'Stepper V1'
                           completedSteps++;
                           upcomingSteps--;
+                          // 'Stepper V2'
                         });
                       }
+                      toggleStep(true);
                     },
                     child: const Text(
                       'Next',
@@ -83,7 +122,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
